a9e45a186e3e114e22db62ef4fdf60b5a9696cb0
Samalieruth01 <samalieruth01@gmail.com>
2024-09-12 17:47:50 +0300

    my first js program
    variables

---
+++ week2/variables.js
@@ -0,0 +1,31 @@
+//innitialization
+var my_name = "samalie";
+
+//declare a variable
+var height;
+
+//assign a variable
+height = 3.4;
+
+console.log(my_name);
+console.log(height);
+
+//let
+let area = 90.5;
+
+f_name = "samalie"
+
+//const
+
+//initialize
+const pie = 3.14;
+
+// declare
+//const k;
+
+k = 34;
+
+//assignment
+pie = 3.15;
+
+//concatinating
